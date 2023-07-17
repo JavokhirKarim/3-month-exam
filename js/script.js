2b343cee448e8f8bf795e72b64a619774339653a
unknown <javohirkarimiy@gmail.com>
2023-07-18 03:24:24 +0500

    backtop

--- js/script.js
+++ js/script.js
@@ -25,3 +25,20 @@ function shrink() {
     navbar.classList.remove("navbar-shrink");
   }
 }
+
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = "20px";
+  } else {
+    backtop.style.bottom = "-50px";
+  }
+}
